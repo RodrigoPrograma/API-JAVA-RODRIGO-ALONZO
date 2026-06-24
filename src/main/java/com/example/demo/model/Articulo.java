055d97f3ad6f224c8df80ac29aceadcc6cfbd2ec
Rodrigo <ralonzo1337@gmail.com>
2026-06-24 14:25:45 -0300

    Añadidos getters y setters al modelo para modificación de articulo

--- src/main/java/com/example/demo/model/Articulo.java
+++ src/main/java/com/example/demo/model/Articulo.java
@@ -14,4 +14,31 @@ public class Articulo {
     private String nombre;
 
     private Double precio;
+    
+    public Articulo() {
+    }   
+
+    public Articulo(String nombre, Double precio) {
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public Double getPrecio() {
+        return precio;
+    }
+    public void setPrecio(Double precio) {
+        this.precio = precio;
+    }
 }
